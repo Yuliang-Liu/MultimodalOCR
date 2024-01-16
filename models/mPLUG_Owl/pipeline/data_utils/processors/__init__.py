15218c3a03dbbc74b5983be193699517f848f234
echo840 <1731396519@qq.com>
2024-01-16 17:26:24 +0800

    dev

--- models/mPLUG_Owl/pipeline/data_utils/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Alibaba. All rights reserved.
-from .builder import PROCESSORS, build_processors
-from .default_processor import DefaultProcessor
-from .caption_processor import CaptionProcessor
-
-__all__ = [
-    'PROCESSORS', 'build_processors',
-    'DefaultProcessor', 'CaptionProcessor'
-]
